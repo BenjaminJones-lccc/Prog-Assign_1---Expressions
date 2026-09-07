f6bea9cade9937cd8906c57396d5cd0e40d01492
BenjaminJones-lccc <benjaminjones2@student.lccc.wy.edu>
2026-09-07 02:39:33 +0000

    Hello World Assignment

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello, World!" << end1;
+    cout << "Hello to the COSC 1030 Course!" << end1;
+    cout << "My name is Benjamin Jones" << end1;
+    cout << "I am in my last year of my major for my associates"  << end1;
+    cout << "I currently like reading and working out quite a lot! Helps keep my mind fresh" << end1;
+    cout << "My favorite movies are both the troy starring brad pitt and the odyssey made by christopher nolan" << end1;
+    cout << "My favorite video game is destiny 2, and my second would be total war troy" << end1;
+    cout << "I am living with my brother and his family, but will probably be moving out soon" << end1;
+    cout << "My brother has a dog whos name is Courtney, and she's basically my best friend." << end1;
+    cout << "I am super psyched to see the differences in this and python and am already over the moon with what we got so far" << end1;
+
+    return 0;
+}
